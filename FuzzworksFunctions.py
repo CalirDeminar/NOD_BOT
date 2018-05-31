45d7ef6edad566865c98a7f8f853fc0d1a97789c
Drago6777 <edward.firespinner@gmail.com>
2018-05-31 12:26:49 +0100

    Added item not found error to keyError exception

--- FuzzworksFunctions.py
+++ FuzzworksFunctions.py
@@ -11,16 +11,19 @@ HUB = {"Jita": "60003760",
 
 
 def get_item_value(name):
-    item_id = Esi.get_item_id(name)
-    print(item_id)
-    look_up_url = "https://market.fuzzwork.co.uk/aggregates/?" + \
-                  "region=" + HUB["Jita"] + "&" \
-                  "types=" + item_id
-    search_res = urllib.request.urlopen(look_up_url)
-    data = json.loads(search_res.read().decode())
-    buy_price = 0
-    buy_price = data[item_id]["buy"]["weightedAverage"]
-    sell_price = 0
-    sell_price = data[item_id]["sell"]["weightedAverage"]
-    return "__**" + name + ":**__\n**Buy Price:** " + '{0:,.2f}'.format(float(buy_price)) +\
-           "\n**Sell Price:** " + '{0:,.2f}'.format(float(sell_price)) + "\n"
+    try:
+        item_id = Esi.get_item_id(name)
+        print(item_id)
+        look_up_url = "https://market.fuzzwork.co.uk/aggregates/?" + \
+                      "region=" + HUB["Jita"] + "&" \
+                      "types=" + item_id
+        search_res = urllib.request.urlopen(look_up_url)
+        data = json.loads(search_res.read().decode())
+        buy_price = 0
+        buy_price = data[item_id]["buy"]["weightedAverage"]
+        sell_price = 0
+        sell_price = data[item_id]["sell"]["weightedAverage"]
+        return "__**" + name + ":**__\n**Buy Price:** " + '{0:,.2f}'.format(float(buy_price)) +\
+               "\n**Sell Price:** " + '{0:,.2f}'.format(float(sell_price)) + "\n"
+    except KeyError:
+        return "Item Not Found"
