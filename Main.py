12aa8fc245b533298fcd394495b4006574b0285d
Drago6777 <edward.firespinner@gmail.com>
2018-08-28 11:31:59 +0100

    limited batphone to single sent command

--- Main.py
+++ Main.py
@@ -155,9 +155,9 @@ async def lastRolled(ctx):
 @bot.command()
 async def batphone(ctx, ping_text):
     target_channel = "440447527905394689"
-    await ctx.send("Ping Sending")
+    # await ctx.send("Ping Sending")
     await ctx.send_message(discord.Object(id=target_channel), "@everyone " + ping_text)
-    await ctx.send("Ping Sent")
+    # await ctx.send("Ping Sent")
 
 
 @bot.event
